7493ece2a54f621814d06ab61dbb8f7eb636b9fd
Anshul Rana <urwithanshul7@gmail.com>
2025-08-11 23:38:16 +0530

    done misc

--- Backend-Part-4/Backend/index.js
+++ Backend-Part-4/Backend/index.js
@@ -30,11 +30,23 @@ app.listen(port , () => {
     console.log(`app is listening on port ${port}`);
 });
 
+app.use(express.urlencoded({extended}));
+app.use(express.json());                 //*two middlewares for parsing
+
 app.get("/register", (req,res) => {   //*app.get ko call lagai hai app ek function hai and oospe get method ko call lagai jaari hai jo register pe accept hori hai
     let {user , password} = req.query;                //*in get  form ka data req.query ke andar aara hota hai
     res.send(`standard get response welcome ${user}`);   //*oos data ko deconstruct kiya hai in the form of object
 });//*res send kar re hain
 
 app.post("/register", (req, res) => {
-    res.send("standard post response");
+    let {user , password} = req.body;
+    res.send(`standard post response ${user}`);
 });
+
+
+
+//?HANDLING POST REQUEST 
+
+//*POST REQ KE ANDAR JO BHI DATA BHEJ TE HAIN VO REQ BODY KE THROUGH JATA HAI
+//*TO HANDLE IT WE HAVE TO SET UP IT AND THEN 
+//*THEN PARSE THE DATA FOR EXPRESS TO READ IT
